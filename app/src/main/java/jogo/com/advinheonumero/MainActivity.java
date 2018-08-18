6e30823f3ea7098777ab8a231625a2e5c25655ce
eduardosgit <eduardosrodrigues@outlook.com>
2018-08-17 23:42:47 -0300

    Inclusao do botao e limpar o numero inserido.

--- app/src/main/java/jogo/com/advinheonumero/MainActivity.java
+++ app/src/main/java/jogo/com/advinheonumero/MainActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnKeyListener{
+public class MainActivity extends Activity {
+//        implements View.OnKeyListener{
     private int numeroSorteado;
     private TextView menssagem;
+    private TextView contador;
     private EditText entrada;
     private int cont;
 
@@ -24,30 +27,47 @@ public class MainActivity extends Activity implements View.OnKeyListener{
         numeroSorteado = (int) (Math.random() *101);
         menssagem = findViewById(R.id.mensagem);
         entrada = findViewById(R.id.entrada);
-        entrada.setOnKeyListener(this);
+        contador = findViewById(R.id.contador);
+        final Button botao = findViewById(R.id.botao);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifica();
+            }
+        });
+//        entrada.setOnKeyListener(this);
     }
 
 
-    @Override
-    public boolean onKey(View v, int KeyCode, KeyEvent event) {
-        if(KeyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP) {
-            verifica();
-            entrada.getText().clear();
-            return true;
-        }
-        return false;
-    }
+//    @Override
+//    public boolean onKey(View v, int KeyCode, KeyEvent event) {
+//        if(KeyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP) {
+//            verifica();
+//            entrada.getText().clear();
+//            return true;
+//        }
+//        return false;
+//    }
 
     private void verifica() {
         int numero = Integer.parseInt(entrada.getText().toString());
         if(numero == numeroSorteado) {
             menssagem.setText(R.string.texto1);
+            cont++;
+            contador.setText(String.valueOf(cont));
+            entrada.getText().clear();
         }
-        if(numero > numeroSorteado) {
+        else if(numero > numeroSorteado) {
             menssagem.setText(R.string.texto2);
+            cont++;
+            contador.setText(String.valueOf(cont));
+            entrada.getText().clear();
         }
-        if(numero < numeroSorteado)  {
+        else  {
             menssagem.setText(R.string.texto3);
+            cont++;
+            contador.setText(String.valueOf(cont));
+            entrada.getText().clear();
         }
     }
 
